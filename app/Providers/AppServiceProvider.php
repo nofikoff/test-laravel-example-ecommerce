62359d857f14b7c23a13eaa294228a827a17cbf0
Novikov <ruslan.n@hypecharge.io>
2025-12-28 17:41:11 +0200

    React. Fix packages version

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,22 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+
+        // Disable "data" wrapper for Inertia.js compatibility
+        JsonResource::withoutWrapping();
     }
 }
